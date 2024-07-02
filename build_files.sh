80be16acc88d617f62616ddf8b2de5100fbb0426
aellun <okello.k.otieno@gmail.com>
2024-07-02 18:30:37 +0300

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
  echo "BUILD START"
- apt-get update && apt-get install -y python3-pip
+ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
