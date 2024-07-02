bc4b29cd42f14d0955967ab5f0e1536803e0eb2f
aellun <okello.k.otieno@gmail.com>
2024-07-02 18:02:34 +0300

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
  echo "BUILD START"
+ apt-get update && apt-get install -y python3-pip
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
