2feff5c63a5fc8ba887238ef452fd1729e8ccb80
aellun <okello.k.otieno@gmail.com>
2024-07-02 18:50:52 +0300

    updated build

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,11 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 
+# Add pip to PATH
+export PATH=/python312/bin:$PATH
+
 # Install project dependencies
 pip install -r requirements.txt
 
 # Run Django collectstatic
-python3 DjangoQuiz/manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
