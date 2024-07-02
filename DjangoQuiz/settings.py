11dced91148d33d7b2aaf97fbe128ac9268aee0b
aellun <okello.k.otieno@gmail.com>
2024-07-02 18:40:02 +0300

    update the contents

--- DjangoQuiz/settings.py
+++ DjangoQuiz/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
- echo "BUILD START"
- curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
+# Install project dependencies
+pip install -r requirements.txt
+
+# Run Django collectstatic
+python3 DjangoQuiz/manage.py collectstatic --noinput
